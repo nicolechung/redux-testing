78b0c9e3e4347087740755d2b51d63ee2ebaf018
Nicole Chung <nicole.chung@gmail.com>
2017-04-02 09:39:58 -0400

    setting up webpack

---
+++ examples/thunks/README.md
@@ -0,0 +1,6 @@
+# Running
+
+In the Terminal, type:
+`../../node_modules/.bin/webpack-dev-server --inline`
+
+And press enter.
---
+++ examples/thunks/webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+    './index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: path.resolve(__dirname, 'dist'),
+    // match the output path
+
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  watch: true
+}
+
