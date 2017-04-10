29fbaadbba5e14ea4c077177965b61f183e54e39
Nicole Chung <nicole.chung@gmail.com>
2017-04-09 21:11:26 -0400

    new webpack files for each example

---
+++ examples/promises/webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    './index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    overlay: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    // match the output path
+    proxy: {
+      '/api': 'http://localhost:3000'
+    },
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  watch: true
+}
+
