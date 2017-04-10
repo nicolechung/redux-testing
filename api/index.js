75182e0eb1f2e1fe76a1a78bcb7fd003a47b97c3
Nicole Chung <nicole.chung@gmail.com>
2017-04-09 21:10:09 -0400

    thunks example, no test yet

--- api/index.js
+++ api/index.js
@@ -5,6 +5,22 @@ app.get('/api', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/api/passionfruit', function (req, res) {
+  const callback = () => {
+    res.status(200)
+    res.send(`Passionate from miles away / Passive with the things you say / Passin' up on my old ways / I can't blame you no, no'`)
+  }
+  setTimeout(callback, 5000)
+})
+
+app.get('/api/passionfruit/fail', function (req, res) {
+  const callback = () => {
+    res.status(500)
+    res.error(`This api called has failed. On purpose`)
+  }
+  setTimeout(callback, 5000)
+})
+
 var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
--- examples/thunks/index.js
+++ examples/thunks/index.js
@@ -1,2 +1,80 @@
-document.write('welcome to my app');
-console.log('Hello World thing!');
+import axios from 'axios'
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {createAction} from 'redux-actions'
+
+const FETCHING_LYRIC = 'FETCHING_LYRIC'
+const FETCH_FAILED = 'FETCH_FAILED'
+const FETCH_SUCCESS = 'FETCH_SUCCESS'
+
+// action
+function fetchLyric () {
+  const request = axios.get('/api/passionfruit')
+
+  return (dispatch, getState) => {
+    dispatch(createAction(`FETCHING_LYRIC`)())
+
+    return request.then(response => response.data)
+    .then((response) => {
+      // FETCH_SUCCESS
+      console.log('----fetch success----')
+      dispatch(createAction(`FETCH_SUCCESS`)(response))
+    })
+    .catch((error) => {
+      // FETCH_FAILED
+      dispatch(createAction(`FETCH_FAILED`)(error))
+    })
+  }
+}
+
+
+// reducer
+const initialState = {
+  isFetching: false,
+  didError: false,
+  lyric: ''
+}
+
+
+
+function reducer (state = initialState, action) {
+  switch (action.type) {
+    case FETCHING_LYRIC:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case FETCH_FAILED:
+      return {
+        ...state,
+        didError: true,
+        isFetching: false
+      }
+    case FETCH_SUCCESS:
+      return {
+        lyric: action.payload,
+        didError: false,
+        isFetching: false
+      }
+    default:
+      return state
+  }
+}
+
+// setup store with redux-thunk middleware, also
+// setup redux dev tool
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(
+    thunkMiddleware
+  ))
+)
+
+// initial state
+console.log(store.getState())
+// dispatch the action we created
+store.dispatch(fetchLyric()).then(() => {
+  console.log(store.getState())
+})
+
