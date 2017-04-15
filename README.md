519ba56c8e7f4858cdace5dc1a3929300f0171d3
Nicole Chung <nicole.chung@gmail.com>
2017-04-14 21:54:42 -0400

    updating api and docs

--- README.md
+++ README.md
@@ -4,7 +4,23 @@ Testing redux using thunks, promises, generators and async/await
 Work in progress (i.e. not a thing yet)
 
 
-# Redux thunk
-When testing with actions that use `fetch`, you can use `nock`
+# Redux thunk - testing libraries
+When testing with actions that use `fetch`, you can use `nock` to mock the request/response.
 
-However when testing with actions that use `axios` you should use `moxios`
+However when testing with actions that use `axios` you should use `moxios` to mock the request/reponse.
+
+# Redux thunk
+- can dispatch many actions from a single action
+
+## Testing and Mocking
+- Have to mock the request and the response object
+- In your action creator, you have more control of your payload, so mocking it is easier
+
+# redux-promise middleware
+- single action returns either the response or the error object from a Promise resolve or reject
+
+## Testing and Mocking
+- redux-promise, on success, returns the response object (not the request promise object).
+- Have to mock the request and reponse object
+- because redux-promise just returns the response or error object, hard to mock discrete stages of the api call (loading, then success/error)
+- because middleware returns the ACTUAL response, not just the part you want, testing can be a bit tricker (in jest you have to use toMatchObject which takes your mock object and compares it to the mock response, which might contain more key/value pairs than you need)
--- api/index.js
+++ api/index.js
@@ -29,6 +29,68 @@ app.get('/api/passionfruit/fail', function (req, res) {
   setTimeout(callback, 5000)
 })
 
+/* something complicated like booking a trip */
+app.get('/api/trip/airline/reservation', function (req, res) {
+  let currentDate = new Date()
+  let futureFlight = new Date(currentDate.getFullYear(),
+                              currentDate.getMonth() + 2,
+                              currentDate.getDay(),
+                              currentDate.getHours() + 3)
+
+  const callback = () => {
+    res.status(200)
+    res.json({
+      airline: 'JetBlue',
+      flightCode: 'B6998',
+      depart: 'Buffalo',
+      departureTime: futureFlight
+    })
+  }
+  setTimeout(callback, 5000)
+})
+
+app.get('/api/trip/hotel/reservation', function (req, res) {
+  let currentDate = new Date()
+  let arrivalDate = new Date(currentDate.getFullYear(),
+                              currentDate.getMonth() + 4,
+                              currentDate.getDay(),
+                              3)
+
+  let departureDate = new Date(currentDate.getFullYear(),
+                              currentDate.getMonth() + 4,
+                              currentDate.getDay() + 7,
+                              3)
+  const callback = () => {
+    res.status(200)
+    res.json({
+      hotel: 'Marriot',
+      arrive: arrivalDate,
+      departure: departureDate
+    })
+  }
+  setTimeout(callback, 2000)
+})
+
+app.get('/api/trip/airport/shuttle', function (req, res) {
+  const callback = () => {
+    res.status(200)
+    res.json({
+      message: 'Shuttle has been booked!'
+    })
+  }
+  setTimeout(callback, 3000)
+})
+
+app.get('/api/trip/bicycle/rental', function (req, res) {
+  const callback = () => {
+    res.status(200)
+    res.json({
+      message: 'Bike rental has been booked!'
+    })
+  }
+  setTimeout(callback, 3000)
+})
+
 var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
